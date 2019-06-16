37fa75ff8d57755cf684a054b91503d96bd86d57
Sam Fullen <sam.fullen@gear4music.com>
2019-06-16 22:04:07 +0100

    Add test to check data returns array of objects (red)

--- src/__tests__/getData.test.js
+++ src/__tests__/getData.test.js
@@ -4,8 +4,16 @@ import chai from 'chai';
 const { should, expect } = chai;
 
 describe('getData()', () => {
-  it('should return an array', async () => {
-    const data = await getData();
+  let data;
+  beforeEach(async () => {
+    data = await getData();
+  });
+
+  it('should return an array', () => {
     expect(data).to.be.a('Array');
+  });
+
+  it('should be an array of objects', () => {
+    expect(data[0]).to.be.a('Object', 'Data should contain objects');
   });
 });
