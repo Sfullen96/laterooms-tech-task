9e39528d54583dedeeca509325065192553adb8f
Sam Fullen <sam.fullen@gear4music.com>
2019-06-16 22:20:07 +0100

    Write test to validate the json schema returned in each object within the data aarray (red)

--- src/__tests__/getData.test.js
+++ src/__tests__/getData.test.js
@@ -1,9 +1,32 @@
 import getData from '../data';
 import chai from 'chai';
 
+chai.use(require('chai-json-schema'));
 const { should, expect } = chai;
 
 describe('getData()', () => {
+  const dataSchema = {
+    title: 'data',
+    type: 'object',
+    required: ['name', 'starRating', 'facilities'],
+    properties: {
+      name: {
+        type: 'string',
+      },
+      starRating: {
+        type: 'number',
+      },
+      facilities: {
+        type: 'array',
+        minItems: 1,
+        uniqueItems: true,
+        items: {
+          type: 'string',
+        },
+      },
+    },
+  };
+
   let data;
   beforeEach(async () => {
     data = await getData();
@@ -15,5 +38,9 @@ describe('getData()', () => {
 
   it('should be an array of objects', () => {
     expect(data[0]).to.be.a('Object', 'Data should contain objects');
+  });
+
+  it('should contain the correct properties', () => {
+    expect(data[0]).to.be.jsonSchema(dataSchema);
   });
 });
