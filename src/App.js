7ae97173694da9f51a248587ab833a647ffbb80e
Sam Fullen <sam.fullen@gear4music.com>
2019-06-17 09:00:21 +0100

    Fetch results in App.js and pass into SearchResults to pass test

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Container } from 'react-bootstrap';
 
 import './App.scss';
 
 import SearchResults from './components/SearchResults/SearchResults';
+import getData from './data';
 
 function App() {
+  const [results, setResults] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await getData();
+      setResults(data);
+    };
+
+    // Simulate an API call
+    setTimeout(() => {
+      fetchData();
+    }, 1500);
+  }, []);
+
   return (
     <Container className="App">
-      <SearchResults />
+      <SearchResults results={results} />
     </Container>
   );
 }
