9cdfde561c18242206ebcb7d639e821771d9dc73
Sam Fullen <sam.fullen@gear4music.com>
2019-06-17 10:12:21 +0100

    Initial filter component test (red)

---
+++ src/__tests__/components/Filter.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import Filter from '../../components/Filter/Filter';
+
+describe('<Filter />', () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallow(<Filter />);
+  });
+
+  it('should render without crashing', () => {
+    expect(wrapper.find('div.filter').length).toEqual(1);
+  });
+});
