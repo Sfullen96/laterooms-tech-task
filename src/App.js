4eb6c22a46126f4c770487cc18c9a0df0ba3a663
Sam Fullen <sam.fullen@gear4music.com>
2019-06-17 08:46:08 +0100

    Render <SearchResults /> in <App />

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import './App.scss';
 
+import SearchResults from './components/SearchResults/SearchResults';
+
 function App() {
-  return <div className="App" />;
+  return (
+    <div className="App">
+      <SearchResults />
+    </div>
+  );
 }
 
 export default App;
--- src/components/SearchResults/SearchResults.js
+++ src/components/SearchResults/SearchResults.js
@@ -1,5 +1,9 @@
 import React from 'react';
 
-const SearchResults = () => <div className="search_results" />;
+const SearchResults = () => (
+  <div className="search_results">
+    <h1>Search Results</h1>
+  </div>
+);
 
 export default SearchResults;
