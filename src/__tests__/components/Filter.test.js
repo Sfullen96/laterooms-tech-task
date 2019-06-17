5cffde7cf5c1eddb819155a7e58932095d6e94d7
Sam Fullen <sam.fullen@gear4music.com>
2019-06-17 10:53:22 +0100

    Test the filter facilities dropdown renders the correct options

--- src/__tests__/components/Filter.test.js
+++ src/__tests__/components/Filter.test.js
@@ -13,5 +13,30 @@ describe('<Filter />', () => {
     expect(wrapper.find('div.filter').length).toEqual(1);
   });
 
-  it('should render a list of facilities', () => {});
+  it('should render a list of facilities', () => {
+    wrapper.setProps({ filters: ['car park', 'gym', 'pool'] });
+
+    expect(wrapper.find('.filter__facilities-dropdown').length).toEqual(1);
+    expect(
+      wrapper
+        .find('.filter__facilities-dropdown')
+        .find('option')
+        .first()
+        .text(),
+    ).toEqual('car park');
+    expect(
+      wrapper
+        .find('.filter__facilities-dropdown')
+        .find('option')
+        .at(1)
+        .text(),
+    ).toEqual('gym');
+    expect(
+      wrapper
+        .find('.filter__facilities-dropdown')
+        .find('option')
+        .at(2)
+        .text(),
+    ).toEqual('pool');
+  });
 });
