45ef95380e87006b6259d084db19dd47398b883f
Sam Fullen <sam.fullen@gear4music.com>
2019-06-17 09:43:51 +0100

    Render the hotel information (green)

--- src/components/SearchResults/SearchResults.js
+++ src/components/SearchResults/SearchResults.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col, Figure } from 'react-bootstrap';
+
+import './SearchResults.scss';
 
 import Loader from '../Loader/Loader';
 
@@ -14,7 +16,27 @@ const SearchResults = ({ results }) => {
       <Row>
         {results.map(result => (
           <Col xs="12" sm="4" key={result.name} className="search_results__result">
-            {result.name}
+            <Figure>
+              <Figure.Image
+                src={result.image}
+                alt={result.name}
+                className="search_results__image"
+                thumbnail
+              />
+              <Figure.Caption>
+                <p className="search_results__name">{result.name}</p>
+                <p className="search_results__rating">{result.starRating}</p>
+                {Boolean(result.facilities.length) && (
+                  <ul className="search_results__facilities">
+                    {result.facilities.map(facility => (
+                      <li key={facility} className="search_results__facility">
+                        {facility}
+                      </li>
+                    ))}
+                  </ul>
+                )}
+              </Figure.Caption>
+            </Figure>
           </Col>
         ))}
       </Row>
